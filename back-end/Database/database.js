e287fb2fa0a1a70997ffae8b28240b949a7e1150
Sameem-Amjad <sameemamjadarsu@gmail.com>
2024-02-23 21:44:50 +0500

    df

--- back-end/Database/database.js
+++ back-end/Database/database.js
@@ -1,7 +1,6 @@
 require('dotenv').config(); 
 const databaseUrl = process.env.DATABASE_URL || "";
 const database_name = process.env.Database_Name;
-console.log(databaseUrl,database_name) 
 const mongoose = require( 'mongoose' );
 mongoose.connect( databaseUrl,{dbName:database_name}, {
   useNewUrlParser: true, 
