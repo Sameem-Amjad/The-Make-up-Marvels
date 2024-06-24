ed9ee87203ed0d2627bb263270746d8edf0a6532
Sameem-Amjad <sameemamjadarsu@gmail.com>
2024-06-25 01:43:35 +0500

    ass

--- back-end/Database/database.js
+++ back-end/Database/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const databaseUrl = process.env.DATABASE_URL || "";
 const database_name = process.env.Database_Name;
 const mongoose = require( 'mongoose' );
-mongoose.connect( databaseUrl,{dbName:database_name}, {
+mongoose.connect( databaseUrl, {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
 }).then(()=>{console.log("database connected successfully") }).catch((e)=>{console.log(e +"sameeem")}); 
